64f20a7352292d1fb715e07c3c66e3e6da95c28c
Vladislav Korbut <v.korbut8@gmail.com>
2016-11-19 23:41:09 +0300

    level changing fix

--- js/ctrl/alphabeth.js
+++ js/ctrl/alphabeth.js
@@ -3,5 +3,6 @@ destApp.controller('alphabethCtrl', function($scope, $http, $rootScope) {
     $scope.alphabeth=$scope.alphabeth.split('');
     $scope.setLevel = function (number) {
     	$rootScope.globalLevel = number-1;
+    	$rootScope.level = 0;
     }
 });
